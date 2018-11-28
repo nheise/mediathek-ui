cfa26fdecdfd1b5ec919cdb407e57ea29f5839d5
Niklas Heise <n.heise@heise-media.de>
2018-11-28 20:12:33 +0100

    first step simple layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,30 @@
 const m = require('mithril')
 
 function mainLayout() {
-  return function(){ return m('div.container', [ m( 'div', 'Hello' ) ] ) }
+  return function(){ return m('div.container', [ m( breadCrum ), m( directory ) ] ) }
 }
 
 function mainLayoutView() {
   return { view: mainLayout() }
+}
+
+const breadCrum = {
+  view: vnode => m('nav', m('ol', [ m('li', m('a[href=#]', '..')), m('li', 'dir1') ] ) )
+}
+
+const files = [
+  {name: 'file1'},
+  {name: 'file2'},
+  {name: 'file3'},
+  {name: 'file4'},
+]
+
+const directory = {
+  view: vnode => m('div.directory', files.map( f => m( file, f ) ) )
+}
+
+const file = {
+  view: vnode => m('a[href=#]', vnode.attrs.name )
 }
 
 m.route.prefix('/mediathek')
